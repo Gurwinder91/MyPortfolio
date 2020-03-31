7901cde498e3c45a12a466bbe75758184742ee87
Gurwinder Singh <GurwinderSingh@thetaxbackgroup.com>
2020-03-31 18:25:57 +0530

    Improve css and added additional features

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -2,7 +2,7 @@ const selector = {
     header: document.querySelector('header'),
     menuIcon: document.querySelector('#menu-icon'),
     displaySmNav: document.querySelector('#display-sm-nav'),
-    closeSidenav: document.querySelector('#display-sm-nav .close'),
+    closeSidenav: document.querySelector('#display-sm-nav .close-icon'),
     loader: document.querySelector('#loader'),
     rootEl: document.querySelector('#root'),
 }

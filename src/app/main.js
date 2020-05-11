c17fd32bd7f18470946451d49aedd2e713bc28ec
Gurwinder Singh <somal.gurwinder7@gmail.com>
2020-05-11 22:12:06 +0530

    Portal design improvements

--- src/app/main.js
+++ src/app/main.js
@@ -1,8 +1,18 @@
+import moment from 'moment';
+
 import '../scss/main.scss';
 import '../assets/my-resume.pdf';
 import '../assets/img/title-logo.svg';
 
 (function () {
+    
+    const calculateExperience = () => {
+        const startedCareer = moment('01/01/2016');
+        const months = moment().diff(startedCareer, 'months');
+        const elements = document.querySelectorAll('.overall-experience');
+        elements.forEach(el => el.textContent = `${(months/12).toFixed(1)} years`); 
+    };
+
     setTimeout(() => {      
         document.querySelector('#loader').remove();
         
@@ -10,6 +20,8 @@ import '../assets/img/title-logo.svg';
         const rootTemplateClone = rootTemplate.content.cloneNode(true);
         document.querySelector('#root').appendChild(rootTemplateClone);
         rootTemplate.remove();
+
+        calculateExperience();
 
         require('./event-manager');
         require('./smooth-scroll');
